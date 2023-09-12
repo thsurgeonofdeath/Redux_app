b933f44c3c75cc7789f6f0f7e77989c4e2e26081
thsurgeonofdeath <salahek25@gmail.com>
2023-09-12 11:35:45 +0100

    redux setup

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import { render } from "react-dom";
 import {BrowserRouter as Router} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App'
+import configureStore from './redux/configureStore'
+import {Provider as ReduxProvider} from 'react-redux'
+
+const store = configureStore();
 
 render(
-<Router>
-  <App />
-</Router>, 
+<ReduxProvider store={store}>
+  <Router>
+    <App />
+  </Router>
+</ReduxProvider>, 
 document.getElementById("app"));
